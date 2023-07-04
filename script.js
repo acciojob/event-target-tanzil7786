4358e21a9c2fafdefcbfe6823f3f289d94d93000
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-07-04 22:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,29 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+		if (!this.listeners.has(event)) {
+			this.listeners.set(event, new Set());
+		}
+    this.listeners.get(event).add(callback);
     }
   
     removeEventListener(event, callback) {
-      
+		if (this.listeners.has(event)) {
+			const eventListeners = this.listeners.get(event);
+			eventListeners.delete(callback);
+			if (eventListeners.size === 0) {
+				this.listeners.delete(event);
+			}
+		}
     }
   
     dispatchEvent(event) {
-    
+		if (this.listeners.has(event)) {
+			const eventListeners = this.listeners.get(event);
+			eventListeners.forEach((callback) => {
+				callback();
+			});
+		}
     }
-    
   }
   
